0fc62c2fb051f0b04b322f0d50061d8f2d381c8e
christian-manalang <christianmanalang0318@gmail.com>
2024-10-07 20:17:32 +0800

    Update Group.py

--- Group.py
+++ Group.py
@@ -10,3 +10,14 @@ st.header("Laptop Prices Dataset")
 df = pd.read_csv("laptop_price.csv")
 
 df.head(10)
+
+nbins = int(np.sqrt(len(df)))
+
+    plt.figure(figsize=(10, 8))
+    plt.hist(df['Price (Euro)'], bins=nbins, edgecolor='black')
+
+    plt.title('Distribution of Laptop Prices', fontsize=12, fontweight='bold')
+    plt.xlabel('Price (Euro)')
+    plt.ylabel('Frequency')
+
+    st.pyplot(plt)
